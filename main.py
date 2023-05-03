7467e428eb904601ab983d8bef186ae2fe0a4c01
HowHowlHowl <gabrielefogu@gmail.com>
2023-05-03 17:15:17 +0200

    codice

--- main.py
+++ main.py
@@ -10,34 +10,56 @@ from scipy.stats import kurtosis, skew
 
 directories_names = ['covidandroidnocough', 'covidandroidwithcough', 'asthmaandroidwithcough', 'healthyandroidnosymp', 'healthyandroidwithcough']
 
+DIR_NAME = 'src/Test/'
 
-def extract_features(array, data_type):
-    # Normalizzazione dell'array fra -1 e 1.
-    # da decide se positiva/inifluente/utile/ecc
-    # array = 2 * (array - np.min(array)) / (np.max(array) - np.min(array)) - 1
-    if data_type == 'mfcc':
-        return (
 
-        )
+def extract_features(audio, sr):
+    onset_env = librosa.onset.onset_strength(y=audio, sr=sr)
 
-    elif data_type == 'stft':
-        return (
+    duration = librosa.get_duration(y=audio)
+    print('duration', duration)
 
-        )
+    times = librosa.times_like(onset_env, sr=sr)
+    onset_frames = librosa.onset.onset_detect(onset_envelope=onset_env, sr=sr)
+    print('onset_frames:', onset_frames)
 
-    else data_type == 'mel':
-        return {
-            'mean': str(np.mean(array)),
-            'std_dev': str(np.std(array)),
-            'min': str(np.min(array)),
-            'max': str(np.max(array)),
-            'e_norm': str(np.linalg.norm(array)),
-            'median': str(np.median(array)),
-            'skew': str(skew(array, axis=None)),
-            'kurt': str(kurtosis(array, axis=None)),
-            'perc25': str(np.percentile(array, 25)),
-            'perc75': str(np.percentile(array, 75))
-        }
+    tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
+    print('tempo:', tempo[0])
+
+    period = np.argmax(librosa.stft(audio))
+    print('period:', period)
+
+    rmse = librosa.feature.rms(y=audio)[0]
+    print('rmse:', rmse)
+
+    sc = librosa.feature.spectral_centroid(y=audio)
+    print('spectral centroid:', sc[0])
+
+    rolloff = librosa.feature.spectral_rolloff(y=audio, sr=sr, roll_percent=0.85)
+    print('rolloff:', rolloff[0])
+
+    zc = librosa.feature.zero_crossing_rate(y=audio)
+    print('zero crossing:', zc[0])
+
+    mfcc = librosa.feature.mfcc(y=audio, sr=sr)
+    mfcc_delta = librosa.feature.delta(mfcc)
+    mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
+    print('mfcc length', np.shape(len(mfcc)) + ' is ' + str(mfcc) + ' each component is long: ' + str(len(mfcc[0])))
+    print('mfcc_delta:', np.shape(mfcc_delta))
+    print('mfcc_delta2:', np.shape(mfcc_delta2))
+
+    # return {
+    #     'mean': str(np.mean(array)),
+    #     'std_dev': str(np.std(array)),
+    #     'min': str(np.min(array)),
+    #     'max': str(np.max(array)),
+    #     'e_norm': str(np.linalg.norm(array)),
+    #     'median': str(np.median(array)),
+    #     'skew': str(skew(array, axis=None)),
+    #     'kurt': str(kurtosis(array, axis=None)),
+    #     'perc25': str(np.percentile(array, 25)),
+    #     'perc75': str(np.percentile(array, 75))
+    # }
 
 
 def is_covid(dir_name):
@@ -64,7 +86,7 @@ def get_mfcc_spec(audio, sr):
 
 
 def load_data():
-    with open('src/Test/files.json', 'r') as f:
+    with open(DIR_NAME + 'files.json', 'r') as f:
         file_dict = json.load(f)
 
     dataset_val = {}
@@ -78,44 +100,48 @@ def load_data():
             covid = is_covid(dir_name)
 
             # BREATH
-            audio_breath, sr = librosa.core.load('src/Test/' + dir_name + '/breath/' + filelist[0])
-
-            # Calcola la STFT e il valore assoluto degli spettri di potenza
-            stft_breath = get_stft_spec(audio_breath)
-
-            # Calcola spettrogramma di Mel
-            mel_breath = get_mel_spect(audio_breath, sr, n_mel=128)
-
-            # COUGH
-            audio_cough, sr = librosa.core.load('src/Test/' + dir_name + '/cough/' + filelist[1])
-
-            # Calcola la STFT e il valore assoluto degli spettri di potenza
-            stft_cough = get_stft_spec(audio_cough)
-
-            # Calcola spettrogramma di Mel
-            mel_cough = get_mel_spect(audio_cough, sr, n_mel=128)
+            audio_breath, breath_sr = librosa.core.load(DIR_NAME + dir_name + '/breath/' + filelist[0])
+            audio_breath, _ = librosa.effects.trim(audio_breath)
 
             dataset_val[dir_name]['breath'].append({
                 'filename': filelist[0],
                 'covid': covid,
-                'stft': extract_features(stft_breath),
-                'mel': extract_features(mel_breath),
+                # 'stft': extract_features(stft_breath),
+                'audio_features': extract_features(audio_breath, breath_sr),
             })
+
+            # Calcola la STFT e il valore assoluto degli spettri di potenza
+            #stft_breath = get_stft_spec(audio_breath)
+
+            # Calcola spettrogramma di Mel
+            #mel_breath = get_mel_spect(audio_breath, sr, n_mel=128)
+
+            # COUGH
+            audio_cough, cough_sr = librosa.core.load(DIR_NAME + dir_name + '/cough/' + filelist[1])
+            audio_cough, _ = librosa.effects.trim(audio_cough)
+
             dataset_val[dir_name]['cough'].append({
                 'filename': filelist[1],
                 'covid': covid,
-                'stft': extract_features(stft_cough),
-                'mel': extract_features(mel_cough),
+                # 'stft': extract_features(stft_cough),
+                'audio_features': extract_features(audio_cough, cough_sr),
             })
+
+            # Calcola la STFT e il valore assoluto degli spettri di potenza
+            #stft_cough = get_stft_spec(audio_cough)
+
+            # Calcola spettrogramma di Mel
+            #mel_cough = get_mel_spect(audio_cough, sr, n_mel=128)
 
     # pp = pprint.PrettyPrinter(indent=4)
     # pp.pprint(dataset_val)
     dict_ = dict(dataset_val)
     json_ = json.dumps(dict_, indent=4)
-    print('json', json_)
+    # print('json', json_)
+
     # SCATENA L'INFERNO:
-    plot(dict_)
-    plt.show()
+    #plot(dict_)
+    #plt.show()
 
     with open('data.json', 'w', encoding='utf-8') as f:
         json.dump(dict(dataset_val), f, ensure_ascii=False, indent=4)
